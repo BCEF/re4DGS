fc96c5298654b1e78700cfbdd1dd2b6333bfd85e
BCEF <schumer425@126.com>
2025-11-09 14:01:00 +0800

    add time

--- scene/gaussian_model.py
+++ scene/gaussian_model.py
@@ -778,9 +778,6 @@ class GaussianModel:
         tmp_radii = self.tmp_radii
         self.tmp_radii = None
 
-        # self._update_base_and_clear_cache()
-        # self.update_deformed_gaussians_for_render()
-
         torch.cuda.empty_cache()
 
     def add_densification_stats(self, viewspace_point_tensor, update_filter):
@@ -855,7 +852,7 @@ class GaussianModel:
         # 最终不透明度 = 空间不透明度 × 时间mask
         # 在logit空间：logit(p1 * p2) = logit(p1) + log(p2)
         # 为了数值稳定，添加小的epsilon
-        self.deformed_opa = base_opacity_logit #+ torch.log(time_mask + 1e-8)
+        self.deformed_opa = base_opacity_logit + torch.log(time_mask + 1e-8)
 
         if self.args.no_dshs:
             self.deformed_shs=self.get_features
@@ -892,7 +889,7 @@ class GaussianModel:
         # 最终不透明度 = 空间不透明度 × 时间mask
         # 在logit空间：logit(p1 * p2) = logit(p1) + log(p2)
         # 为了数值稳定，添加小的epsilon
-        self.deformed_opa = base_opacity_logit #+ torch.log(time_mask + 1e-8)
+        self.deformed_opa = base_opacity_logit + torch.log(time_mask + 1e-8)
         
         if self.args.no_dshs:
             self.deformed_shs=self.get_features.detach()
